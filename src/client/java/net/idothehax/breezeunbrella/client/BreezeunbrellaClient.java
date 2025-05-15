2a6175973e5afd538938f9ed1e4df81e5cee1453
IDoTheHax <hax@idothehax.com>
2025-05-15 17:17:13 +0200

    Init Project

---
+++ src/client/java/net/idothehax/breezeunbrella/client/BreezeunbrellaClient.java
@@ -0,0 +1,10 @@
+package net.idothehax.breezeunbrella.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BreezeunbrellaClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/idothehax/breezeunbrella/Breezeunbrella.java
@@ -0,0 +1,10 @@
+package net.idothehax.breezeunbrella;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Breezeunbrella implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
